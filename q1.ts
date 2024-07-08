5d4d9d860bda3964425f712e2914eddbcb2ccb10
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-07-08 20:07:51 +0700

    660612137

--- q1.ts
+++ q1.ts
@@ -2,8 +2,22 @@
 import axios from "axios";
 
 /* assign interface/type to the function definition properly */
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+
 const getUser = async (userId: number) => {
-  /* Your code here */
+  try {
+    const response = await axios.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const user = response.data;
+    return `${user.name}`;
+  } catch (error) {
+    return "INVALID USER ID";
+  }
 };
 
 //test case
